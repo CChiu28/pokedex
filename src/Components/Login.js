eb8de770b8f5d759dc76b5b52207ceb4c6ca860c
cchiu28 <scorpiusdx@gmail.com>
2022-12-21 03:07:33 -0500

    changes to Login modal

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,12 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { getAuth,createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
-import { Modal, Button, Form, FloatingLabel, Tabs, Tab } from "react-bootstrap";
+import { Modal, Button, Tabs, Tab } from "react-bootstrap";
+import Input from "./Input";
 
 export default function Login(props) {
-    const [input,setInput] = useState({
-        email: '',
-        password: ''
-    });
     const auth = getAuth();
 
     async function handleLogin(e) {
@@ -23,36 +20,12 @@ export default function Login(props) {
         <Modal show={props.show} onHide={props.setModal}>
             <Modal.Header closeButton>Register/Login</Modal.Header>
             <Modal.Body>
-                <Tabs defaultActiveKey='login' fill>
+                <Tabs defaultActiveKey='login' fill className="d-flex flex-row">
                     <Tab eventKey='register' title='Register'>
-                        <Form onSubmit={handleRegister}>
-                            <Form.Group>
-                                <FloatingLabel label="Email address">
-                                    <Form.Control type="email" placeholder="name@example.com" required />
-                                </FloatingLabel>
-                            </Form.Group>
-                            <Form.Group>
-                                <FloatingLabel label="Password">
-                                    <Form.Control type="password" placeholder="Password" required />
-                                </FloatingLabel>
-                            </Form.Group>
-                            <Button variant="primary" type="submit">Register</Button>
-                        </Form>
+                        <Input handleInput={handleRegister} input={"Register"} />
                     </Tab>
                     <Tab eventKey="login" title='Login'>
-                        <Form onSubmit={handleLogin}>
-                            <Form.Group>
-                                <FloatingLabel label="Email address">
-                                    <Form.Control type="email" placeholder="name@example.com" required />
-                                </FloatingLabel>
-                            </Form.Group>
-                            <Form.Group>
-                                <FloatingLabel label="Password">
-                                    <Form.Control type="password" placeholder="Password" required />
-                                </FloatingLabel>
-                            </Form.Group>
-                            <Button variant="primary" type="submit">Login</Button>
-                        </Form>
+                        <Input handleInput={handleLogin} input={"Login"} />
                     </Tab>
                 </Tabs>
             </Modal.Body>
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -31,10 +31,10 @@ export default function NavBar(props) {
                 <Nav>
                     <Link className="m-1 align-self-center" to='/'>Search</Link>
                     <Link className="m-1 align-self-center" to='/teambuilder'>Team Builder</Link>
-                    <Nav.Item className="m-1">
-                    {userHeader!=='' ? <Button variant="primary" onClick={logOut}>Logout</Button>
-                        : <Nav.Link onClick={() => setOpenModal(true)}>Register/Login</Nav.Link>}
-                    </Nav.Item>
+                    {/* <Nav.Item className="m-1"> */}
+                    {userHeader!=='' ? <Button className="m-1" variant="primary" onClick={logOut}>Logout</Button>
+                        : <Button className="m-1" onClick={() => setOpenModal(true)}>Register/Login</Button>}
+                    {/* </Nav.Item> */}
                 </Nav>
                 <Login show={openModal} setModal={handleClose} getLogin={props.getLogin} />
             </Container>
